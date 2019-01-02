7179c8893ad165a0fe5abcd8aabc1f6ce80cbff4
MEGUMI SHIMABUKURO <shimabukuromeg@gmail.com>
2019-01-02 13:47:11 +0900

    複数のパラメーターを用いた初期化を隔離する

---
+++ RevealingReferencesRefactoringModule.rb
@@ -0,0 +1,46 @@
+# Gearが外部インターフェースの一部の場合
+module SomeFramework
+  class Gear
+    attr_reader :chainring, :cog, :wheel
+
+    def initialize(chainring, cog, wheel)
+      @chainring = chainring
+      @cog = cog
+      @wheel = wheel
+    end
+
+    def gear_inches
+      ratio * wheel.diameter
+    end
+
+    def ratio
+      chainring / cog.to_f
+    end
+  end
+end
+
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim,tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+# 外部のインターフェースをラップし、自身を変更から守る
+module GearWrapper
+  def self.gear(args)
+    SomeFramework::Gear.new(args[:chainring], args[:cog], args[:wheel])
+  end
+end
+
+puts GearWrapper.gear(:chainring => 52, :cog => 11, :wheel => Wheel.new(26, 1.5)).gear_inches
